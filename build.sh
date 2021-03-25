40a899e03969c4c0fc5b352242587667097eada8
devcui <devcui@outlook.com>
2021-03-25 23:08:54 +0800

    :sparkles: all
    build and flush

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-./toolchain-sdcc/bin/sdcc --help
+./tool-sdcc/bin/sdcc ./src/led/led1.c -o /Users/ferried/Projects/mcs51/out/out.ihx
--- flush.sh
+++ flush.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-python3 ./tool-stcgal/stcgal.py --help
+echo "查看USB，请更换成自己的USB串口"
+ls /dev/tty.*
+echo "默认串口为/dev/tty.usbserial-14120"
+python3 ./tool-stcgal/stcgal.py -a -P stc89 -b 9600 -p /dev/tty.usbserial-14120 out/out.ihx
